d82bb1ebf971f81bb038435f8b39134ab66878f1
wey282 <lev.daniel.sar@gmail.com>
2024-10-27 17:51:23 +0200

    logging error server side

--- server/server.js
+++ server/server.js
@@ -117,6 +117,13 @@ io.on('connect', (socket) => {
   });
 });
 
+io.engine.on("connection_error", (err) => {
+  console.log(err.req);      // the request object
+  console.log(err.code);     // the error code, for example 1
+  console.log(err.message);  // the error message, for example "Session ID unknown"
+  console.log(err.context);  // some additional error context
+});
+
 // initSocketIOserver();
 
 if (process.env.NODE_ENV === "production") {
